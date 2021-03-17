4036fbed9e6f137a0ca598ee0feb28338f8a43b7
Vanderlei Almeida <valmeida@uolinc.com>
2021-03-17 02:06:14 -0300

    ajuste no deploy

--- food-request-api/scripts/cluster.sh
+++ food-request-api/scripts/cluster.sh
@@ -7,9 +7,7 @@ source "${ROOT}"/constants.sh
 gcloud container clusters create "$CLUSTER_NAME" \
 --num-nodes 1 \
 --enable-autorepair \
---zone "$CLUSTER_ZONE" \
---additional-zones="$ADDITIONAL_CLUSTER_ZONE" \
---service-account="food-request-demo-node-sa@$PROJECT".iam.gserviceaccount.com \
+--zone "$CLUSTER_ZONE"
 
 # Setting up .kube/config. This happens normally if you don't use --async
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$CLUSTER_ZONE"
